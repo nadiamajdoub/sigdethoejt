c320fe7d1dd2948620c78645a94f5bc5751496b5
“nadiamajdoub” <[nadia.fie@hotmail.com]>
2024-02-05 09:23:28 +0100

    ændringer i småfejl

--- menu.js
+++ menu.js
@@ -1,5 +1,6 @@
 function toggleMenu() {
-    var menu = document.getElementById("nav-links");
-    menu.classList.toggle("show");
-  }
-   
+  var menu = document.querySelector(".nav-links");
+  var burgerMenu = document.querySelector(".burger-menu");
+  menu.classList.toggle("show");
+  burgerMenu.classList.toggle("active");
+}
